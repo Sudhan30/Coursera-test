bd3f1fbd77d572945f5a5a8bebd3c1d41adf123f
Sudhan30 <sudhann.raj@gmail.com>
2016-04-17 12:15:18 +0530

    My Module3 AssignmentV4

--- Module3-Solution/js/script.js
+++ Module3-Solution/js/script.js
@@ -1,13 +1,9 @@
-/*
-
-    We are using the following attributes on anchors in order to collapse the responsive menu when an item is clicked:
-    
-        data-toggle="collapse"
-        data-target=".in"
-
-*/
 $(document).ready(function () {
-    $("nav").find("li").on("click", "a", function () {
-        $('.navbar-collapse.in').collapse('hide');
-    });
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
 });
+
+        $(document).on('click', function() {
+            $('.collapse').collapse('hide');
+        })
