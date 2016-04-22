05cd3cc36f0959b547a9e3147ae40e981c6798f9
Sudhan30 <sudhann.raj@gmail.com>
2016-04-22 23:17:04 +0530

    FinalAssgn

--- Lecture63/js/script.js
+++ Lecture63/js/script.js
@@ -273,6 +273,35 @@ function insertItemPortionName(html,
   return html;
 }
 
+function randomCategoryShortName() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+          test =JSON.parse(xhttp.responseText);
+
+           /*for (var key in test) {
+       if (test.hasOwnProperty(key)) {
+          console.log(test[key].short_name);
+       }
+
+    }*/
+      var max= test.length;
+      var min= 1;
+      random= Math.floor((Math.random() * (max - min) + min));
+      console.log(random);
+      console.log(test[random].short_name)
+
+          document.getElementById("Raj").innerHTML= test.short_name;
+
+      }
+        };
+        xhttp.open("GET","http://davids-restaurant.herokuapp.com/categories.json", true);
+        xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+        xhttp.send();
+        return test[random].short_name;
+}
+
+
 
 global.$dc = dc;
 
