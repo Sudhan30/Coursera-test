a43abddca0bf2c771c2b4da554524062564ce14a
Rajasekaran <439069@CTS.COM>
2016-04-22 16:27:24 +0530

    TestAijax

--- TestAijax3/js/aijax.js
+++ TestAijax3/js/aijax.js
@@ -8,6 +8,9 @@ document.addEventListener("DOMContentLoaded",
   		xhttp.onreadystatechange = function() {
     	if (xhttp.readyState == 4 && xhttp.status == 200) {
       		document.getElementById("Sudhan").innerHTML = xhttp.responseText;
+           test =JSON.parse(xhttp.responseText);
+           console.log(test);
+           document.getElementById("Raj").innerHTML= test;
     	}
   			};
   			xhttp.open("GET", "./Data/name.json", true);
